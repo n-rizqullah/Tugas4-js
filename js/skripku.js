4df0aa5e778964254ef86c434b4738211661e70d
n-rizqullah <naupalrizqullah83@gmail.com>
2023-03-17 14:52:17 +0700

    upload tugas 4 js

---
+++ js/skripku.js
@@ -0,0 +1,12 @@
+function login() {
+    var user = document.getElementById("username").value
+    var pass = document.getElementById("password").value
+
+    if (user == "mimin" && pass == "mimin123") {
+        alert("Selamat Datang Mimin, Anda Administrator")
+    } else if(user == "budi" && pass == "budi123") {
+        alert("Selamat Datang Budi, Anda Operator")
+    } else {
+        alert("Maaf Anda Gagal Login")
+    }
+}
